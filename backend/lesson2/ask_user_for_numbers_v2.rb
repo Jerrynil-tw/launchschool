9a7b1449f4ce1ce469d2d2ef0d93432bda90c25e
洪璽凱 <tpanakin@gmail.com>
2016-10-18 12:33:53 +0800

    add ask_user_for_numbers_v2.rb

---
+++ backend/lesson2/ask_user_for_numbers_v2.rb
@@ -0,0 +1,16 @@
+while user wants to keep going
+  -ask user for the collection of numbers
+  -iterate the collection of numbers one by one
+  -save the first value as starting value
+  -for each iteration, compare saved value with current value
+  -if saved value is greater or its the same
+    -move to the next value in the collection of numbers
+  -else 
+    -assign current value to the saved value
+  -end 
+
+  after iterating through the collection of numbers
+  -ask user if they want to put in another collection of numbers
+end
+
+return the saved list of numbers

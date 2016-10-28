21637e1096fadd37d545ceafb4c26d88f735ed9e
洪璽凱 <tpanakin@gmail.com>
2016-10-28 12:43:30 +0800

    modify mortgage_calculator_v2.rb

--- backend/lesson2/mortgage_calculator_v2.rb
+++ backend/lesson2/mortgage_calculator_v2.rb
@@ -9,7 +9,7 @@ loop do
   loop do
     prompt("How much money do you want to loan?(in dollar)")
     loan_amount = Kernel.gets().chomp()
-    if loan_amount.empty?() || loan_amount.to_f() < 0
+    if loan_amount.empty?() || loan_amount.to_f() <= 0
       prompt("the loan amount doesn't look right,please put in again")
     else
       break
@@ -18,27 +18,27 @@ loop do
 
   loan_amount = loan_amount.to_f()
 
-  prompt("the loan amount is #{loan_amount} dollars")
+  prompt("loan amount = #{format('%.2f', loan_amount)} dollars")
 
   annual_percentage_rate = ""
   loop do
     prompt("How much is Annual Percentage Rate?(put in the number without %)")
     annual_percentage_rate = Kernel.gets().chomp()
-    if annual_percentage_rate.empty?() || annual_percentage_rate.to_f() < 0
-      prompt("the Annual Percentage Rate doesn't look right,please put in again")
+    if annual_percentage_rate.empty?() || annual_percentage_rate.to_f() <= 0
+      prompt("Annual Percentage Rate doesn't look right,please put in again")
     else
       break
     end
   end
 
   monthly_interest_rate = annual_percentage_rate.to_f() / 12
-  prompt("the monthly interest rate is #{monthly_interest_rate} percent")
+  prompt("monthly interest rate = #{format('%.2f', monthly_interest_rate)}%")
 
   yearly_loan_duration = ""
   loop do
     prompt("How long is the loan duration?(in years)")
     yearly_loan_duration = Kernel.gets().chomp()
-    if yearly_loan_duration.empty?() || yearly_loan_duration.to_f() < 0
+    if yearly_loan_duration.empty?() || yearly_loan_duration.to_f() <= 0
       prompt("the yearly loan duration doesn't look right,please put in again")
     else
       break
@@ -46,17 +46,28 @@ loop do
   end
 
   monthly_loan_duration = yearly_loan_duration.to_f() * 12
-  prompt("the monthly loan duration is #{monthly_loan_duration} months")
+  prompt("loan duration = #{format('%.2f', monthly_loan_duration)} months")
 
   monthly_payment = loan_amount * (monthly_interest_rate / 100 / (1 - (1 + monthly_interest_rate / 100)**-monthly_loan_duration))
 
-  prompt("your monthly patment should be #{monthly_payment} dollars")
+  prompt("your monthly payment = #{format('%.2f', monthly_payment)}$")
 
-  prompt("do you want to calculate another mortgage? put in 'y' if you do")
+  answer = ""
+  loop do
+    prompt("do you want to calculate again? put in 'y' if you do,or 'n' if not")
+    answer = Kernel.gets().chomp().to_s()
 
-  answer = Kernel.gets().chomp().to_s()
-
-  break unless answer.downcase().start_with?("y")
+    if answer.downcase().start_with?("y", "n")
+      break
+    else
+      prompt("your type doesn't look right,please put in again")
+    end
+  end
+  if answer.downcase().start_with?("y")
+    next
+  elsif answer.downcase().start_with?("n")
+    break
+  end
 end
 
 prompt("goodbye!")

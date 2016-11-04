8a27f3a44a3ab0718b94f6f9a4bf0c434631795e
洪璽凱 <tpanakin@gmail.com>
2016-11-04 16:30:41 +0800

    modify e1q10.rb

--- backend/programming_foundations/lesson3/e1q10.rb
+++ backend/programming_foundations/lesson3/e1q10.rb
@@ -1,8 +1,21 @@
+# solution 1
 flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
 
 flintstones_hash = {}
 flintstones.each_with_index do |name, index|
   flintstones_hash[name] = index
+end
+
+p flintstones_hash
+
+# solution 2
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+
+flintstones_hash = {}
+
+flintstones.each do |name|
+  flintstones_hash[name] = flintstones.index(name)
 end
 
 p flintstones_hash

0ef683d441d1716011feb4035271a3b805268880
洪璽凱 <tpanakin@gmail.com>
2016-11-04 10:16:40 +0800

    move lesson2 directory to  programming_foundations

--- backend/lesson2/ask_user_for_numbers_v1.rb
+++
@@ -1,7 +0,0 @@
-while user wants to keep going
-  -ask user for the collection of numbers
-  -extract the largest number from the collection of numbers
-  -ask user if they want to put in another collection of numbers
-end
-
-return the saved list of numbers

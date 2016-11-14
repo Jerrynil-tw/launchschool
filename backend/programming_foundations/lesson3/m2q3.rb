cc4c5755fd3811728f3a603da8120373115213e6
洪璽凱 <tpanakin@gmail.com>
2016-11-14 09:50:09 +0800

    fix m2q3.rb

--- backend/programming_foundations/lesson3/m2q3.rb
+++ backend/programming_foundations/lesson3/m2q3.rb
@@ -1,12 +1,12 @@
 def add_name(string_name, array_name)
   string_name += "Jerry Nil"
   array_name << "Jerry Nil"
+
+  p string_name
+  p array_name
 end
 
 string_words = "Welcome! "
 array_words = ["Welcome!"]
 
 add_name(string_words, array_words)
-
-puts "let's say #{string_words}"
-puts "let's say #{array_words}"

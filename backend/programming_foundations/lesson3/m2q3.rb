4b5e82010b0efbb1fd46f6273334775e671c84c1
洪璽凱 <tpanakin@gmail.com>
2016-11-14 10:45:49 +0800

    add  second solution to m2q3.rb

--- backend/programming_foundations/lesson3/m2q3.rb
+++ backend/programming_foundations/lesson3/m2q3.rb
@@ -1,5 +1,7 @@
+# solution 1
+
 def no_tricky_method(a_string_param, an_array_param)
-  a_string_param += "rutabaga"
+  a_string_param << "rutabaga"
   an_array_param << "rutabaga"
 
   p a_string_param
@@ -9,4 +11,20 @@ end
 my_string = "pumpkins "
 my_array = ["pumpkins"]
 no_tricky_method(my_string, my_array)
+
+# solution 2
+
+def no_tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param += ["rutabaga"]
+
+  return a_string_param, an_array_param
+end
+
+my_string = "pumpkins "
+my_array = ["pumpkins"]
+my_string, my_array = no_tricky_method(my_string, my_array)
+
+p my_string
+p my_array
 
